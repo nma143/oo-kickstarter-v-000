8f8156d1086a1611335dc8024aa15c88edae7e7d
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 23:27:00 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,12 @@
 class Backer
 
-attr_accessor :backed_projects
-attr_reader :name
+  attr_accessor :backed_projects
+  attr_reader :name
 
-def initialize(name)
-
-@name = name
-@backed_projects = []
-
-end
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
 
 
 end
